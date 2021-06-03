6244c8b011f55606112d318aeb12288156c76692
AhmadALJallad <>
2021-06-03 21:10:37 +0000

    css done after a bad expriance the the language i should work more on the css

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var like = "Do you like books?Yes or No?";
 var wlc = "Welcome to Cool guys Group!";
 var out = "out now!";
-var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to another world on his way home from the convenience store!, Now tell me what is your favorite novel or book?" 
+var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to a0nother world on his way home from the convenience store!, Now tell me what is your favorite novel or book?" 
 var ans = prompt(like)
 while(ans.toUpperCase() != ("YES" || "NO")){
   if (ans.toUpperCase() === "YES") {
     alert(wlc = "Welcome to the Group") ;}
   else{alert(out = "out now!");var ans = prompt(like) ;}}
-  var ans2 = prompt(fav);
+
 var ans2 = prompt(fav)
 function myFunction() {
   switch (Math.floor(Math.random() * 5)) {
@@ -27,12 +27,7 @@ function myFunction() {
       alert("' Still Life' by Louise Penny");
         break;
     default:
-      alert("no chance for default");
-            }
-            
-            }
-
-
+      alert("no chance for default");}}
 var q = "how much do you like books?enter numerical number";
 var num = prompt(q)
 for(i=0; num !== 0 ;--num){
