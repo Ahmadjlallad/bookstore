60c694816c2b1cb5ed03302bfdde39766da4f55b
AhmadALJallad <>
2021-06-03 12:49:21 +0000

    js for lab 04 add for and while lop

---
+++ app.js
@@ -0,0 +1,37 @@
+var like = "Do you like books?Yes or No?";
+var wlc = "Welcome to Cool guys Group!";
+var out = "out now!";
+var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to another world on his way home from the convenience store!, Now tell me what is your favorite novel or book?" 
+var ans = prompt(like)
+while(ans.toUpperCase() != ("YES" || "NO")){
+  if (ans.toUpperCase() === "YES") {
+    alert(wlc = "Welcome to the Group") ;}
+  else{alert(out = "out now!");var ans = prompt(like) ;}}
+var ans2 = prompt(fav);
+function myFunction(){
+  switch (Math.floor(Math.random() * 5)) {
+    case 0:
+      alert("' Luster' by Raven Leilani");
+        break;
+    case 1:
+      alert("' The Underground Railroad' by Colson Whitehead");
+        break;
+    case 2:
+      alert("'Waterland' by Graham Swift");
+      break;
+    case 3:
+      alert("A Promised Land' by Barack Obama");
+        break;
+    case 4:
+      alert("' Still Life' by Louise Penny");
+        break;
+    default:
+      alert("no chance for default");
+            }
+            
+            }
+
+var q = "how much do you like books?enter numerical number";
+var num = prompt(q)
+for(i=0; num !== 0 ;--num){
+document.write('<div><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573841248-419TWZk7luL.jpg?crop=1xw:0.987xh;center,top&resize=768:*" width="300" height="500" >' +"The Testaments: The Sequel to The Handmaid's Tale"+ "</div>")}
