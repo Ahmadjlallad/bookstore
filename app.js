f6ebdd6c8daea1f9b0487d051ea5c4238e52cf98
AhmadALJallad <>
2021-06-04 12:22:12 +0000

    fixed js missing { and i deleted line 13 yesterday now ever thaing working like charm

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var like = "Do you like books?Yes or No?";
 var wlc = "Welcome to Cool guys Group!";
 var out = "out now!";
 var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to a0nother world on his way home from the convenience store!, Now tell me what is your favorite novel or book?" 
+
+
 var ans = prompt(like)
 while(ans.toUpperCase() != ("YES" || "NO")){
   if (ans.toUpperCase() === "YES") {
-    alert(wlc = "Welcome to the Group") ;}
-  else{alert(out = "out now!");var ans = prompt(like) ;}}
+    alert(wlc = "Welcome to the Group")
+  }
+  else{alert(out = "out now!")}
+  var ans = prompt(like)
+}
+
 
 var ans2 = prompt(fav)
 function myFunction() {
@@ -31,6 +37,5 @@ function myFunction() {
 var q = "how much do you like books?enter numerical number";
 var num = prompt(q)
 for(i=0; num !== 0 ;--num){
-document.write('<div><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573841248-419TWZk7luL.jpg?crop=1xw:0.987xh;center,top&resize=768:*" width="300" height="500" >' +"The Testaments: The Sequel to The Handmaid's Tale"+ "</div>")}
-
-document.write("<h2>" + ans2 + "</h2>" );
+  document.write('<div><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573841248-419TWZk7luL.jpg?crop=1xw:0.987xh;center,top&resize=768:*" width="300" height="500" >' +"The Testaments: The Sequel to The Handmaid's Tale"+ "</div>")}
+document.write("<h3>" + ans2 + "</h3>");
