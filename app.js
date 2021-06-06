28b1407b387887b3f591cdedd1c38a300b3f51a2
AhmadALJallad <>
2021-06-06 09:10:41 +0000

    add function to js

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+// vars start
 var like = "Do you like books?Yes or No?";
 var wlc = "Welcome to Cool guys Group!";
 var out = "out now!";
-var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to another world on his way home from the convenience store!, Now tell me what is your favorite novel or book?" 
+var fav= "My favorite novel “Re:Zero − Starting Life in Another World”. It is a is a Japanese light novel series written by Tappei Nagatsuki.The story centres on Subaru Natsuki, Subaru Natsuki who suddenly finds himself transported to another world on his way home from the convenience store!, Now tell me what is your favorite novel or book?"; 
+var q = "how much do you like books?enter numerical number";
+// vars end
 
 
-var ans = prompt(like)
-while(ans.toUpperCase() != ("YES" || "NO")){
-  if (ans.toUpperCase() === "YES") {
+function askuser(){
+  var ans = prompt(like)
+  while(ans.toUpperCase() != ("YES" || "NO")) {
+    if (ans.toUpperCase() === "YES") {
     alert(wlc = "Welcome to the Group") ;}
-  else{alert(out = "out now!");}
-  var ans = prompt(like)}var ans2 = prompt(fav)
+   else{alert(out = "out now!");var ans = prompt(like)}
+  }
+}
+
+
+function fav_one() {
+  var ans2 = prompt(fav);
   document.write("<h3>" + ans2 + "</h3>");
+}
+
+
 function myFunction() {
   switch (Math.floor(Math.random() * 5)) {
     case 0:
@@ -29,8 +41,17 @@ function myFunction() {
       alert("'Still Life' by Louise Penny");
         break;
     default:
-      alert("no chance for default");}}
-var q = "how much do you like books?enter numerical number";
-var num = prompt(q)
-for(i=0; num !== 0 ;--num){
-  document.write('<div><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573841248-419TWZk7luL.jpg?crop=1xw:0.987xh;center,top&resize=768:*" width="300" height="500" >' +"The Testaments: The Sequel to The Handmaid's Tale"+ "</div>")}
+      alert("no chance for default");}
+}
+
+
+function question() {
+  var num = prompt(q)
+  for(i=0; num !== 0 ;--num){
+    document.write('<div><img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1573841248-419TWZk7luL.jpg?crop=1xw:0.987xh;center,top&resize=768:*" width="300" height="500" >' +"The Testaments: The Sequel to The Handmaid's Tale"+ "</div>")}
+}
+
+
+askuser();
+fav_one();
+question();
